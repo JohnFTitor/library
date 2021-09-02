36bd9e898ad12f093db41662c032af7189c1d728
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-09-02 17:33:51 -0500

    Added Index attribute to the book object

--- script/main.js
+++ script/main.js
@@ -1,5 +1,8 @@
-let myLibrary = [];
 const booksContainer = document.querySelector("#booksContainer");
+const newBook = document.querySelector("#addBook");
+
+let myLibrary = [];
+let index = 0;
 
 function Books(title, author, pages, isRead){
     this.title = title;
@@ -19,36 +22,47 @@ function addBookToLibrary(title, author, pages, isRead) {
 
 function displayBook(){
     myLibrary.forEach((book) => {
-        //Define pages elements
-        const bookDiv = document.createElement('div');
-        const title = document.createElement('p');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const isRead = document.createElement('p');
-        
-        //Add classes
-        bookDiv.classList.add('book');
-        title.classList.add('info');
-        author.classList.add('info');
-        pages.classList.add('info');
-        isRead.classList.add('info');
-
-        //Add object information
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = book.pages;
-        isRead.textContent = book.info();
-
-        //Append childs in the right order
-        bookDiv.appendChild(title);
-        bookDiv.appendChild(author);
-        bookDiv.appendChild(pages);
-        bookDiv.appendChild(isRead);
-
-        //Finally, append the book card to the father
-        booksContainer.appendChild(bookDiv);       
+        if (!('index' in book)){
+            //Define pages elements
+            const bookDiv = document.createElement('div');
+            const title = document.createElement('p');
+            const author = document.createElement('p');
+            const pages = document.createElement('p');
+            const isRead = document.createElement('p');
+            
+            //Add classes
+            bookDiv.classList.add('book');
+            title.classList.add('info');
+            author.classList.add('info');
+            pages.classList.add('info');
+            isRead.classList.add('info');
+    
+            //Add object information
+            title.textContent = `Title: ${book.title}`;
+            author.textContent = `Author: ${book.author}`;
+            pages.textContent = `${book.pages} pages`;
+            isRead.textContent = book.info();
+    
+            //Append childs in the right order
+            bookDiv.appendChild(title);
+            bookDiv.appendChild(author);
+            bookDiv.appendChild(pages);
+            bookDiv.appendChild(isRead);
+    
+            //Adding index to the book
+            index++;
+            book.index = index;
+            bookDiv.id = index;
+    
+            //Finally, append the book card to the father
+            booksContainer.appendChild(bookDiv);       
+        }
     })
 }
+
+
+
+
 
 addBookToLibrary("The Hobbit", "Don't Know", 458, false);
 addBookToLibrary("The Master", "John Strabusky", 558, false);
